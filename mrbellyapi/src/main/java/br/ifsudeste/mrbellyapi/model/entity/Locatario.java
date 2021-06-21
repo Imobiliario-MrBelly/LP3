cd9c77082899dd9b43f6aafbef0600a6349f3e18
rennan <rennandamiao@gmail.com>
2021-06-21 13:31:08 -0300

    LocatarioOK

--- mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/model/entity/Locatario.java
+++ mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/model/entity/Locatario.java
@@ -1,16 +1,15 @@
 package br.ifsudeste.mrbellyapi.model.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
 
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+
 
 public class Locatario extends Pessoa{
-String status;
+
 }
