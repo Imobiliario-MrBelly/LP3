e0f6a3cdf2105bdcccc9ceaed780fa557bed920d
rennan <rennandamiao@gmail.com>
2021-06-29 16:56:14 -0300

    correção da extensão da regra de negocio

--- mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/api/exception/RegradeNegocioException.java
+++ mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/api/exception/RegradeNegocioException.java
@@ -1,6 +1,6 @@
 package br.ifsudeste.mrbellyapi.api.exception;
 
-public class RegradeNegocioException extends  Exception{
+public class RegradeNegocioException extends  RuntimeException{
    public RegradeNegocioException(String msg){
        super(msg);
    }
