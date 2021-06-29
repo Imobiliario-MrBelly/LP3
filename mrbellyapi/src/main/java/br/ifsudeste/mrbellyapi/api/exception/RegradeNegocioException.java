106c30c4676a49ad00f348834ac5c667f0545306
rennan <rennandamiao@gmail.com>
2021-06-29 16:20:09 -0300

    Regra de Negócio

---
+++ mrbellyapi/src/main/java/br/ifsudeste/mrbellyapi/api/exception/RegradeNegocioException.java
@@ -0,0 +1,8 @@
+package br.ifsudeste.mrbellyapi.api.exception;
+
+public class RegradeNegocioException extends  Exception{
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
